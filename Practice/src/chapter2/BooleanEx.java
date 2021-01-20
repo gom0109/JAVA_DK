6df2774097f495f76a11c3f791ec0196dd5991d2
gom0109 <75201349+gom0109@users.noreply.github.com>
2021-01-20 18:21:38 +0900

    210120

---
+++ Practice/src/chapter2/BooleanEx.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class BooleanEx {
+
+	public static void main(String[] args) {
+		boolean isGood = true;
+		System.out.println(isGood);
+
+	}
+
+}
---
+++ Practice/src/chapter2/Variable2.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class Variable2 {
+
+	public static void main(String[] args) {
+		int level = 10;//4byte 기본자료형
+		System.out.println(level);
+
+	}
+
+}
